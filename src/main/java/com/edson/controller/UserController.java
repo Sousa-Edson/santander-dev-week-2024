8104e507643d05d83bccecbabff48711e1e541d8
Edson Sousa <edson3711.es@gmail.com>
2024-04-19 15:31:48 -0300

    separa classe de excessao

--- src/main/java/com/edson/controller/UserController.java
+++ src/main/java/com/edson/controller/UserController.java
@@ -2,6 +2,12 @@ package com.edson.controller;
 
 import com.edson.domain.model.User;
 import com.edson.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -25,6 +31,11 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new user", description = "Create a new user and return the created user's data")
+    @ApiResponses(value = { 
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "422", description = "Invalid user data provided")
+    })
     ResponseEntity<User> create(@RequestBody User useToCreate) {
         var userCreated = userService.create(useToCreate);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userCreated.getId()).toUri();
