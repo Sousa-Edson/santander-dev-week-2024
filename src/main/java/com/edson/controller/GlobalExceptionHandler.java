74105cfd439027b90bb10676cd8d2f0ba30c30f5
Edson Sousa <edson3711.es@gmail.com>
2024-04-19 15:39:49 -0300

    removi as classes parece que nao funciona

--- src/main/java/com/edson/controller/GlobalExceptionHandler.java
+++ src/main/java/com/edson/controller/GlobalExceptionHandler.java
@@ -7,18 +7,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.edson.service.exception.BusinessException;
+
 import java.util.NoSuchElementException;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+ 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
         return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
+      
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
@@ -27,7 +30,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException) {
         var message="Unexpected server error, see the logs";
-        logger.error(message, unexpectedException);
+        LOGGER.error(message, unexpectedException);
                 return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
